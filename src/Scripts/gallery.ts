464979680ecf90978eecd5b08704162aa33a287a
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-03-21 17:26:22 +0530

    added hover

---
+++ src/Scripts/gallery.ts
@@ -0,0 +1,10 @@
+export default()=>{
+    // console.log(3)
+    // const sp = document.querySelectorAll('.sp1-image') ;
+    // sp.forEach((sps)=>{
+    //     sps.onmouseover=
+
+    // }
+    // );
+    
+}
