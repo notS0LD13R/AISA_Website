12e6916aee59d9cbeee552bc3a95c2f601fc96e8
notS0LD13R <joelsebin@gmail.com>
2023-03-24 23:02:40 +0530

    Updated carousel content

--- src/Scripts/Loader.ts
+++ src/Scripts/Loader.ts
@@ -1,6 +1,8 @@
 export default ()=>{
     window.addEventListener('load',async ()=>{
         document.querySelector('.loader')?.classList.toggle('tran')
+        
         document.querySelector('.loader')?.classList.toggle('active')
+        
     })
 }
